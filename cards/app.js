a9ae73c8bb68e6f2410cb6be75c21a468ebae391
bishop <bishop@krouts.com>
2023-06-01 23:04:34 -0400

    refactor async/await

--- cards/app.js
+++ cards/app.js
@@ -1,35 +1,30 @@
-// let deckId;
+let deckId = '';
 
-// fetch('https://deckofcardsapi.com/api/deck/new/draw/?count=1')
-// 	.then(response => response.json())
-// 	.then(data => {
-// 		deckId = data.deck_id;
-// 		let card = data.cards[0];
-// 		console.log(`${card.value} of ${card.suit}`);
-// 	    return fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`);
-// 	})
-// 	.then(response => response.json())
-// 	.then(data => {
-// 		let card = data.cards[0];
-// 		console.log(`${card.value} of ${card.suit}`)
-// 	});
-let deckId;
+// This function gets a new deck from the API and stores the deck id
+async function getDeck() {
+  const response = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
+  const data = await response.json();
+  deckId = data.deck_id;
+}
 
-fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-	.then(response => response.json())
-	.then(data => {
-		deckId = data.deck_id;
-	});
+// This function gets a card from the deck with the deck id stored earlier
+async function drawCard() {
+  const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`);
+  const data = await response.json();
+  const card = data.cards[0];
+  console.log(`${card.value} of ${card.suit}`);
+}
 
-    function drawCard() {
-		fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
-		  .then(response => response.json())
-		  .then(data => {
-			if(data.remaining == 0) {
-			  document.getElementById("card").innerHTML = "No more cards in deck";
-			} else {
-			  let card = data.cards[0];
-			  document.getElementById("card").innerHTML = `${card.value} of ${card.suit}`;
-			}
-		  });
-	  }
+// When the page loads, get a new deck
+window.addEventListener('load', () => {
+  getDeck().then(() => {
+    console.log('New deck of cards ready!');
+  });
+});
+
+// When the button is clicked, draw a card
+document.querySelector('#draw').addEventListener('click', () => {
+  drawCard().then(() => {
+    console.log('Card drawn!');
+  });
+});
